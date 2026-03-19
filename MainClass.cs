af76a5a9bac85c285b7e9333b58ff2861db8d7d4
Meet Rajpal <meetrajpal.linkedin@gmail.com>
2026-03-19 19:31:36 +0530

    Upto half Practical 3

--- MainClass.cs
+++ MainClass.cs
@@ -4,8 +4,14 @@
     {
         public static void Main(String[] args)
         {
+            // ========== Practical 1 ==========
             //Practical1.main([]);
-            Practical2.main([]);
+
+            // ========== Practical 2 ==========
+            //Practical2.main([]);
+
+            // ========== Practical 3 ==========
+            Practical3.main([]);
         }
     }
 }
---
+++ Practical3.cs
@@ -0,0 +1,41 @@
+﻿namespace sim_cs_practicals
+{
+    class Practical3
+    {
+        class Sponsor
+        {
+            protected string _owner = "Rakuteen";
+        }
+
+        class Team : Sponsor
+        {
+            string TeamName  { get; set; }
+
+            public Team(string teamName)
+            {
+                this.TeamName = teamName;
+            }
+
+            /*
+             * 
+             This method will print _owner value and TeamName value and will return nothing.
+             */
+            public void printInfo()
+            {
+                Console.WriteLine($"Owner: {this._owner}, Team Name: {this.TeamName}");
+            }
+
+        }
+
+        class Bird
+        {
+
+        }
+
+        public static void main(String[] args)
+        {
+            Team teamObj = new Team("CSK");
+            teamObj.printInfo();
+        }
+    }
+}
