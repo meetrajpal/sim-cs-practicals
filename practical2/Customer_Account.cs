656ed823daba27ac6e5f96ade7c2a99b269c6059
Meet Rajpal <meetrajpal.linkedin@gmail.com>
2026-03-24 18:08:28 +0530

    updated practical 2 as per mentor review

---
+++ practical2/Customer_Account.cs
@@ -0,0 +1,26 @@
+﻿namespace sim_cs_practicals.practical2
+{
+    class Customer_Account
+    {
+        public string Bank_Name { get; set; }
+        public long Customer_AccountNo { get; set; }
+
+        public string Customer_Name { get; set; }
+
+        public Customer_Account(long accNo, string custName)
+        {
+            this.Customer_AccountNo = accNo;
+            this.Customer_Name = custName;
+        }
+
+        /// <summary>
+        /// This method will return void / nothing and it will print all the information about customer account.
+        /// </summary>
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Customer name: {this.Customer_Name}");
+            Console.WriteLine($"Account number: {this.Customer_AccountNo}");
+            Console.WriteLine($"Bank name: {this.Bank_Name}");
+        }
+    }
+}
---
+++ practical2/Practical2.cs
@@ -0,0 +1,11 @@
+﻿namespace sim_cs_practicals.practical2
+{
+    class Practical2
+    {
+        public static void main(String[] args)
+        {
+            Customer_Account custAcc = new Customer_Account(132564987L, "Meet Rajpal") { Bank_Name = "BOB" };
+            custAcc.PrintInfo();
+        }
+    }
+}
