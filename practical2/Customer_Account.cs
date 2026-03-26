62c949ac8c93449a7ca8d8acc19d686a1e5ebfa0
Meet Rajpal <meetrajpal.linkedin@gmail.com>
2026-03-26 13:23:36 +0530

    fixed namespace according to latest practice and specifed the access modifier for the classes

--- practical2/Customer_Account.cs
+++ practical2/Customer_Account.cs
@@ -1,26 +1,26 @@
-﻿namespace sim_cs_practicals.practical2
+﻿namespace practical2;
+
+internal class Customer_Account
 {
-    class Customer_Account
+    public string Bank_Name { get; set; }
+    public long Customer_AccountNo { get; set; }
+
+    public string Customer_Name { get; set; }
+
+    public Customer_Account(long accNo, string custName)
     {
-        public string Bank_Name { get; set; }
-        public long Customer_AccountNo { get; set; }
+        this.Customer_AccountNo = accNo;
+        this.Customer_Name = custName;
+        this.Bank_Name = "";
+    }
 
-        public string Customer_Name { get; set; }
-
-        public Customer_Account(long accNo, string custName)
-        {
-            this.Customer_AccountNo = accNo;
-            this.Customer_Name = custName;
-        }
-
-        /// <summary>
-        /// This method will return void / nothing and it will print all the information about customer account.
-        /// </summary>
-        public void PrintInfo()
-        {
-            Console.WriteLine($"Customer name: {this.Customer_Name}");
-            Console.WriteLine($"Account number: {this.Customer_AccountNo}");
-            Console.WriteLine($"Bank name: {this.Bank_Name}");
-        }
+    /// <summary>
+    /// This method will return void / nothing and it will print all the information about customer account.
+    /// </summary>
+    public void PrintInfo()
+    {
+        Console.WriteLine($"Customer name: {this.Customer_Name}");
+        Console.WriteLine($"Account number: {this.Customer_AccountNo}");
+        Console.WriteLine($"Bank name: {this.Bank_Name}");
     }
 }
--- practical2/Program.cs
+++ practical2/Program.cs
@@ -1,11 +1,10 @@
-﻿namespace sim_cs_practicals.practical2
+﻿namespace practical2;
+
+internal class Program
 {
-    class Program
+    public static void Main(String[] args)
     {
-        public static void Main(String[] args)
-        {
-            Customer_Account custAcc = new Customer_Account(132564987L, "Meet Rajpal") { Bank_Name = "BOB" };
-            custAcc.PrintInfo();
-        }
+        Customer_Account custAcc = new Customer_Account(132564987L, "Meet Rajpal") { Bank_Name = "BOB" };
+        custAcc.PrintInfo();
     }
 }
